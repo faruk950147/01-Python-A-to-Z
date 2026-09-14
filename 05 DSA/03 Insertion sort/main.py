0c3d31ecd8b0476a1f14292fc35fbc7856d9083f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-14 07:18:48 -0700

    I have some changes insertion sort

--- 05 DSA/03 Insertion sort/main.py
+++ 05 DSA/03 Insertion sort/main.py
@@ -915,7 +915,7 @@ class InsertionSort:
                 # Decrement j to move to the previous element 
                 # that means we are comparing the key with the next element on the left side of the sorted portion.
                 j -= 1
-
+                print(f"Shifting {arr[j + 1]} to the right, j = {j}")
             # Insert key into its correct position
             arr[j + 1] = key
 
