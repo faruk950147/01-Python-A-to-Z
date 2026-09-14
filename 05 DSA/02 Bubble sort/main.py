72099725caf4165577948d9bb08e153df50fa92c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-14 09:43:47 -0700

    I have some changes insertion sort

--- 05 DSA/02 Bubble sort/main.py
+++ 05 DSA/02 Bubble sort/main.py
@@ -532,7 +532,18 @@
             n = len(arr)
 
             for i in range(n - 1):
-
+                # i = 0
+                # j = i - 1
+                # = 0 - 1
+                # = -1
+                # i = 1
+                # j = i - 1
+                # = 1 - 1
+                # = 0
+                i = 2
+                j = i - 1
+                j = 2 - 1
+                j = 1
                 for j in range(n - i - 1):
 
                     if arr[j] > arr[j + 1]:
@@ -1331,6 +1342,18 @@ class BubbleSort:
             swapped = False
 
             # Last i elements are already in place
+            # i = 0
+            # j = i - 1
+            # = 0 - 1
+            # = -1
+            # i = 1
+            # j = i - 1
+            # = 1 - 1
+            # = 0
+            i = 2
+            j = i - 1
+            j = 2 - 1
+            j = 1
             for j in range(n - i - 1):
                 if arr[j] > arr[j + 1]:
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
