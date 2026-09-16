b4a1f2fe7d26caad03b787a14dbd7670f2f67533
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-15 22:10:52 -0700

    I have some changes sort

---
+++ 03. Intermediate Level/01 Built-in/lists/impotance.py
@@ -0,0 +1,9 @@
+lst = [10, 20, 30, 40, 50]
+# print(len(lst))
+# print(lst[len(lst) - 1])
+
+# mid = len(lst) // 2 # Find the middle index
+# print(lst[mid]) # Print the middle element
+
+mid = lst[len(lst) // 2] # Find the middle element directly
+print(mid) # Print the middle element
