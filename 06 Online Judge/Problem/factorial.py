3b48c1e2976f710aaf839bbe6425c950b0a694ee
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-18 07:03:38 -0700

    I have some changes sort

--- 06 Online Judge/Problem/factorial.py
+++ 06 Online Judge/Problem/factorial.py
@@ -12,20 +12,6 @@ class Factorial:
 
         return fact
 
-
-    # iterative
-    def factorial_iterative(self, n):
-        if n < 0:
-            return "Factorial is not defined for negative numbers"
-
-        fact = 1
-
-        for i in range(1, n + 1):
-            fact *= i
-
-        return fact
-
-
     # recursive
     def factorial_recursive(self, n):
         if n < 0:
