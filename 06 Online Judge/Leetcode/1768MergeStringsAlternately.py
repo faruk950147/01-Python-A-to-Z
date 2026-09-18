f267cbceefe6e6d43bd3299dd7b8e3983fe6a162
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-18 09:51:57 -0700

    I have some changes sort

--- 06 Online Judge/Leetcode/1768MergeStringsAlternately.py
+++ 06 Online Judge/Leetcode/1768MergeStringsAlternately.py
@@ -1,17 +1,43 @@
 class Solution:
+
     def mergeAlternately(self, word1, word2):
         result = []
+
         max_length = max(len(word1), len(word2))
 
         for i in range(max_length):
+
             if i < len(word1):
                 result.append(word1[i])
+
             if i < len(word2):
                 result.append(word2[i])
 
         return ''.join(result)
 
-# Test locally
-sol = Solution()
-print(sol.mergeAlternately("abc", "pqr"))  # Output: apbqcr
+    def merge_the_tools(self, string, k):
 
+        for i in range(0, len(string), k):
+
+            # k determines the chunk size and loop step.
+            # string[i:i+k] extracts up to k characters.
+            part = string[i:i+k]
+
+            # Remove duplicate characters while preserving
+            # the order of their first occurrence.
+            print(''.join(sorted(set(part), key=part.index)))
+
+
+if __name__ == '__main__':
+
+    # Test locally
+    sol = Solution()
+
+    print(sol.mergeAlternately("abc", "pqr"))
+    # Output: apbqcr
+
+    sol.merge_the_tools("AABCAAADA", 3)
+    # Output:
+    # AB
+    # CA
+    # AD
