367b1367eb6442714fb13a0112c45914f3e1e916
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-19 01:59:00 -0700

    I have some changes sort

--- 06 Online Judge/Problem/advanceTopic.py
+++ 06 Online Judge/Problem/advanceTopic.py
@@ -46,9 +46,11 @@ def longestCommonPrefix(strs):
 
     for i in range(len(strs[0])):
         char = strs[0][i]
+        print(f"Characters: {char}")
 
         for word in strs[1:]:
             if i >= len(word) or word[i] != char:
+                print(f"strs[0][:i]: {i}")
                 return strs[0][:i]
 
     return strs[0]
