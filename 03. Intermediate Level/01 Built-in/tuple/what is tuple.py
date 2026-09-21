beb46c9ab8766a138b6446c954746a5654e83ebf
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-20 19:56:25 -0700

    I have some changes sort

--- 03. Intermediate Level/01 Built-in/tuple/what is tuple.py
+++ 03. Intermediate Level/01 Built-in/tuple/what is tuple.py
@@ -6,6 +6,6 @@
 # when the run output show ordered, unchangeable, and allows duplicate values, it means that the tuple is ordered, unchangeable, and allows duplicate values.
 # Tuple is immutable (change not possible) but its items are immutable only if they themselves are immutable.
 # Tuple is iterable (loop possible).
-tuple = (1,2,3, [4,5,6]) # itq is mutable (change possible) because its item is mutable.
+tuple = (1,2,3, [4,5,6]) # it is mutable (change possible) because its item is mutable.
 print("\nTuple:", tuple)
 
