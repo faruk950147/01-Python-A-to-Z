278ebb43bb14b7c39e264aa1309143156f232d40
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-22 06:46:14 +0600

    I have some changes sort

--- 03. Intermediate Level/01 Built-in/dictionary/problems/marks.py
+++ 03. Intermediate Level/01 Built-in/dictionary/problems/marks.py
@@ -1,35 +1,28 @@
-studentsMarks = {
-    "faruk": {
-        "details":
-            {
-                "roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}
-            }
-        },
-    "tom": {
-        "details":
-            {
-                "roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}
-            }
-        },
-    "jerry": {
-        "details":
-            {
-                "roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}
-            }
-        }
+'''
+class Student:
+    def __init__(self, name, record):
+        self.name = name
+        self.roll = record["details"]["roll"]
+        self.dept = record["details"]["dept"]
+        self.marks = record["details"]["marks"]
+
+    def percentage(self):
+        return sum(self.marks.values()) / len(self.marks)
+
+students_dict = {
+    "faruk": {"details": {"roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
+    "tom": {"details": {"roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
+    "jerry": {"details": {"roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}
 }
-for student in studentsMarks:
-    # printed the name of the student
-    # print(student)
-    # printed the details of the student
-    # print(studentsMarks[student])
-    # printed the marks of the student
-    length = len(studentsMarks[student]["details"]["marks"])
-    percentage = sum(studentsMarks[student]["details"]["marks"].values()) / length
-    print(f"{student} : {percentage:.2f} %")
+
+students = [Student(name, data) for name, data in students_dict.items()]
+
+for student in students:
+    print(f"{student.name} (Roll: {student.roll}, Dept: {student.dept}) : {student.percentage():.2f} %")
+
+'''
 
 
-''' 
 def calculate_percentages(students):
     for student in students:
         marks = students[student]["details"]["marks"]
@@ -37,35 +30,12 @@ def calculate_percentages(students):
         percentage = sum(marks.values()) / length
         print(f"{student} : {percentage:.2f} %")
 
-studentsMarks = {
+students = {
     "faruk": {"details": {"roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
     "tom": {"details": {"roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
     "jerry": {"details": {"roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}
 }
 
-calculate_percentages(studentsMarks)
+calculate_percentages(students)
 
-
-class Student:
-    def __init__(self, name, details):
-        self.name = name
-        self.details = details
-
-    def percentage(self):
-        marks = self.details["marks"]
-        return sum(marks.values()) / len(marks)
-
-studentsMarks = {
-    "faruk": {"details": {"roll": 101, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
-    "tom": {"details": {"roll": 102, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}, 
-    "jerry": {"details": {"roll": 103, "dept": "CSE", "marks": {"C": 85, "C++": 80, "Python": 85, "DS": 90, "DBMS": 88, "OS": 92, "CN": 95}}}
-}
-
-students = [Student(name, data["details"]) for name, data in studentsMarks.items()]
-
-for student in students:
-    print(f"{student.name} : {student.percentage():.2f} %")
-    
-    
-'''
 
