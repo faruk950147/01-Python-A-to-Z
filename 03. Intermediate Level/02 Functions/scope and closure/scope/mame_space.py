2b92a45717105d7f870c52584ce8116a688dccc0
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-22 21:32:03 +0600

    I have some changes sort

--- 03. Intermediate Level/02 Functions/scope and closure/scope/mame_space.py
+++ 03. Intermediate Level/02 Functions/scope and closure/scope/mame_space.py
@@ -1,3 +1,18 @@
+# =============== Class Space =================
+class Test:
+    def __init__(self):
+        self.num = 10
+
+    def display(self):
+        print(f"Class Scope: {dir(self)}")
+
+# =============== Global Space =================
+print(f"Global Scope: {dir()}")
+test = Test()
+test.display()
+print(f"Global Scope: {dir()}")
+
+
 # =============== Function Space =================
 # print(f"Initial Scope: {dir()}")
 # def test():
