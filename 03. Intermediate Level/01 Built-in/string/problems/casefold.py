742c78c61d8483962a898714677e6a8a4d714590
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-22 16:30:23 +0600

    I have some changes sort

--- 03. Intermediate Level/01 Built-in/string/problems/casefold.py
+++ 03. Intermediate Level/01 Built-in/string/problems/casefold.py
@@ -1,7 +1,7 @@
-# def casefold_string(string):
-#     return string.casefold()
+def casefold_string(string):
+    return string.casefold()
 
-# print(casefold_string("Hello"))
+print(casefold_string("Hello"))
 
 def casefold_string(string1, string2):
     return string1.casefold() == string2.casefold()
