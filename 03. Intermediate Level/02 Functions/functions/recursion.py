6a988b75f1145d938e5c2dfbca0d28731e912a79
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-09-26 10:40:52 +0600

    I have some changes sort

--- 03. Intermediate Level/02 Functions/functions/recursion.py
+++ 03. Intermediate Level/02 Functions/functions/recursion.py
@@ -202,8 +202,25 @@ fib(5)
 
 Notice:
 
-    fib(3)
-    fib(2)
+fib(2) = fib(1) + fib(0)
+       = 1 + 0
+       = 1
+
+fib(3) = fib(2) + fib(1)
+       = 1 + 1
+       = 2
+
+fib(4) = fib(3) + fib(2)
+       = 2 + 1
+       = 3
+
+fib(5) = fib(4) + fib(3)
+       = 3 + 2
+       = 5
+
+fib(6) = fib(5) + fib(4)
+       = 5 + 3
+       = 8
 
 and other subproblems are calculated repeatedly.
 
